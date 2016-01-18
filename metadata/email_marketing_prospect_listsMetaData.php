f6f32b7dcc6c00ab059f5a945a11bd9f855977fc
Butschster <butschster@gmail.com>
2016-01-18 21:24:14 +0300

    Update php syntax

--- metadata/email_marketing_prospect_listsMetaData.php
+++ metadata/email_marketing_prospect_listsMetaData.php
@@ -1,33 +1,35 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (! defined('sugarEntry') || ! sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
  * Free Software Foundation with the addition of the following permission added
  * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
  * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
  * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- * 
+ *
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo. If the display of the logo is not reasonably feasible for
@@ -35,65 +37,62 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * "Powered by SugarCRM".
  ********************************************************************************/
 
-$dictionary['email_marketing_prospect_lists'] = array ( 
-
-	'table' => 'email_marketing_prospect_lists',
-
-	'fields' => array (
-		array (
-			'name' => 'id',
-			'type' => 'varchar',
-			'len' => '36',
-		),
-		array (
-			'name' => 'prospect_list_id',
-			'type' => 'varchar',
-			'len' => '36',
-		),
-		array (
-			'name' => 'email_marketing_id',
-			'type' => 'varchar',
-			'len' => '36',
-		),
-        array (
-			'name' => 'date_modified',
-			'type' => 'datetime'
-		),
-		array (
-			'name' => 'deleted',
-			'type' => 'bool',
-			'len' => '1',
-			'default' => '0'
-		),
-	),
-	'indices' => array (
-		array (
-			'name' => 'email_mp_listspk',
-			'type' => 'primary',
-			'fields' => array ( 'id' )
-		),
-		array (
-			'name' => 'email_mp_prospects',
-			'type' => 'alternate_key',
-			'fields' => array (	'email_marketing_id',
-								'prospect_list_id'
-						)
-		),
-	),
-	
- 	'relationships' => array (
-		'email_marketing_prospect_lists' => array(
-											'lhs_module'=> 'EmailMarketing', 
-											'lhs_table'=> 'email_marketing', 
-											'lhs_key' => 'id',
-											'rhs_module'=> 'ProspectLists', 
-											'rhs_table'=> 'prospect_lists', 
-											'rhs_key' => 'id',
-											'relationship_type'=>'many-to-many',
-											'join_table'=> 'email_marketing_prospect_lists', 
-											'join_key_lhs'=>'email_marketing_id',
-											'join_key_rhs'=>'prospect_list_id', 
-		),
-	)
-)
-?>
+$dictionary['email_marketing_prospect_lists'] = [
+    'table'         => 'email_marketing_prospect_lists',
+    'fields'        => [
+        [
+            'name' => 'id',
+            'type' => 'varchar',
+            'len'  => '36',
+        ],
+        [
+            'name' => 'prospect_list_id',
+            'type' => 'varchar',
+            'len'  => '36',
+        ],
+        [
+            'name' => 'email_marketing_id',
+            'type' => 'varchar',
+            'len'  => '36',
+        ],
+        [
+            'name' => 'date_modified',
+            'type' => 'datetime'
+        ],
+        [
+            'name'    => 'deleted',
+            'type'    => 'bool',
+            'len'     => '1',
+            'default' => '0'
+        ],
+    ],
+    'indices'       => [
+        [
+            'name'   => 'email_mp_listspk',
+            'type'   => 'primary',
+            'fields' => ['id']
+        ],
+        [
+            'name'   => 'email_mp_prospects',
+            'type'   => 'alternate_key',
+            'fields' => [
+                'email_marketing_id',
+                'prospect_list_id'
+            ]
+        ],
+    ],
+    'relationships' => [
+        'email_marketing_prospect_lists' => [
+            'lhs_module'        => 'EmailMarketing',
+            'lhs_table'         => 'email_marketing',
+            'lhs_key'           => 'id',
+            'rhs_module'        => 'ProspectLists',
+            'rhs_table'         => 'prospect_lists',
+            'rhs_key'           => 'id',
+            'relationship_type' => 'many-to-many',
+            'join_table'        => 'email_marketing_prospect_lists',
+            'join_key_lhs'      => 'email_marketing_id',
+            'join_key_rhs'      => 'prospect_list_id',
+        ],
+    ]
+];
--- metadata/foldersMetaData.php
+++ metadata/foldersMetaData.php
@@ -1,33 +1,35 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (! defined('sugarEntry') || ! sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
  * Free Software Foundation with the addition of the following permission added
  * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
  * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
  * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- * 
+ *
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo. If the display of the logo is not reasonably feasible for
@@ -35,171 +37,164 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * "Powered by SugarCRM".
  ********************************************************************************/
 
-/*********************************************************************************
+$dictionary['folders'] = [
+    'table'   => 'folders',
+    'fields'  => [
+        [
+            'name'     => 'id',
+            'type'     => 'id',
+            'required' => true,
+        ],
+        [
+            'name'     => 'name',
+            'type'     => 'varchar',
+            'len'      => 25,
+            'required' => true,
+        ],
+        [
+            'name'    => 'folder_type',
+            'type'    => 'varchar',
+            'len'     => 25,
+            'default' => null,
+        ],
+        [
+            'name'     => 'parent_folder',
+            'type'     => 'id',
+            'required' => false,
+        ],
+        [
+            'name'    => 'has_child',
+            'type'    => 'bool',
+            'default' => '0',
+        ],
+        [
+            'name'    => 'is_group',
+            'type'    => 'bool',
+            'default' => '0',
+        ],
+        [
+            'name'    => 'is_dynamic',
+            'type'    => 'bool',
+            'default' => '0',
+        ],
+        [
+            'name' => 'dynamic_query',
+            'type' => 'text',
+        ],
+        [
+            'name'     => 'assign_to_id',
+            'type'     => 'id',
+            'required' => false,
+        ],
 
- * Description:
- * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc. All Rights
- * Reserved. Contributor(s): ______________________________________..
- *********************************************************************************/
+        [
+            'name'     => 'created_by',
+            'type'     => 'id',
+            'required' => true,
+        ],
+        [
+            'name'     => 'modified_by',
+            'type'     => 'id',
+            'required' => true,
+        ],
+        [
+            'name'    => 'deleted',
+            'type'    => 'bool',
+            'default' => '0',
+        ],
+    ],
+    'indices' => [
+        [
+            'name'   => 'folderspk',
+            'type'   => 'primary',
+            'fields' => ['id']
+        ],
+        [
+            'name'   => 'idx_parent_folder',
+            'type'   => 'index',
+            'fields' => ['parent_folder']
+        ],
+    ],
+];
 
-$dictionary['folders'] = array(
-	'table' => 'folders',
-	'fields' => array(
-		array(
-			'name'			=> 'id',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'name',
-			'type'			=> 'varchar',
-			'len'			=> 25,
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'folder_type',
-			'type'			=> 'varchar',
-			'len'			=> 25,
-			'default'		=> NULL,
-		),
-		array(
-			'name'			=> 'parent_folder',
-			'type'			=> 'id',
-			'required'		=> false,
-		),
-		array(
-			'name'			=> 'has_child',
-			'type'			=> 'bool',
-			'default'		=> '0',
-		),
-		array(
-			'name'			=> 'is_group',
-			'type'			=> 'bool',
-			'default'		=> '0',
-		),
-		array(
-			'name'			=> 'is_dynamic',
-			'type'			=> 'bool',
-			'default'		=> '0',
-		),
-		array(
-			'name'			=> 'dynamic_query',
-			'type'			=> 'text',
-		),
-		array(
-			'name'			=> 'assign_to_id',
-			'type'			=> 'id',
-			'required'		=> false,
-		),
+$dictionary['folders_subscriptions'] = [
+    'table'   => 'folders_subscriptions',
+    'fields'  => [
+        [
+            'name'     => 'id',
+            'type'     => 'id',
+            'required' => true,
+        ],
+        [
+            'name'     => 'folder_id',
+            'type'     => 'id',
+            'required' => true,
+        ],
+        [
+            'name'     => 'assigned_user_id',
+            'type'     => 'id',
+            'required' => true,
+        ],
+    ],
+    'indices' => [
+        [
+            'name'   => 'folders_subscriptionspk',
+            'type'   => 'primary',
+            'fields' => ['id']
+        ],
+        [
+            'name'   => 'idx_folder_id_assigned_user_id',
+            'type'   => 'index',
+            'fields' => ['folder_id', 'assigned_user_id']
+        ],
+    ],
+];
 
-		array(
-			'name'			=> 'created_by',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'modified_by',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'deleted',
-			'type'			=> 'bool',
-			'default'		=> '0',
-		),
-	),
-	'indices' => array(
-		array(
-			'name'			=> 'folderspk',
-			'type'			=> 'primary',
-			'fields'		=> array('id')
-		),
-		array(
-			'name'			=> 'idx_parent_folder',
-			'type'			=> 'index',
-			'fields'		=> array('parent_folder')
-		),
-	),
-);
-
-$dictionary['folders_subscriptions'] = array(
-	'table' => 'folders_subscriptions',
-	'fields' => array(
-		array(
-			'name'			=> 'id',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'folder_id',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'assigned_user_id',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-	),
-	'indices' => array(
-		array(
-			'name'			=> 'folders_subscriptionspk',
-			'type'			=> 'primary',
-			'fields'		=> array('id')
-		),
-		array(
-			'name'			=> 'idx_folder_id_assigned_user_id',
-			'type'			=> 'index',
-			'fields'		=> array('folder_id', 'assigned_user_id')
-		),
-	),
-);
-
-$dictionary['folders_rel'] = array(
-	'table' => 'folders_rel',
-	'fields' => array(
-		array(
-			'name'			=> 'id',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'folder_id',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'polymorphic_module',
-			'type'			=> 'varchar',
-			'len'			=> 25,
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'polymorphic_id',
-			'type'			=> 'id',
-			'required'		=> true,
-		),
-		array(
-			'name'			=> 'deleted',
-			'type'			=> 'bool',
-			'default'		=> '0',
-		),
-	),
-	'indices' => array(
-		array(
-			'name'			=> 'folders_relpk',
-			'type'			=> 'primary',
-			'fields'		=> array('id'),
-		),
-		array(
-			'name'			=> 'idx_poly_module_poly_id',
-			'type'			=> 'index',
-			'fields'		=> array('polymorphic_module', 'polymorphic_id'),
-		),
-		array(
-		    'name'			=> 'idx_fr_id_deleted_poly',
-		    'type'			=> 'index',
-		    'fields'		=> array('folder_id','deleted','polymorphic_id'),
-		),
-	),
-);
+$dictionary['folders_rel'] = [
+    'table'   => 'folders_rel',
+    'fields'  => [
+        [
+            'name'     => 'id',
+            'type'     => 'id',
+            'required' => true,
+        ],
+        [
+            'name'     => 'folder_id',
+            'type'     => 'id',
+            'required' => true,
+        ],
+        [
+            'name'     => 'polymorphic_module',
+            'type'     => 'varchar',
+            'len'      => 25,
+            'required' => true,
+        ],
+        [
+            'name'     => 'polymorphic_id',
+            'type'     => 'id',
+            'required' => true,
+        ],
+        [
+            'name'    => 'deleted',
+            'type'    => 'bool',
+            'default' => '0',
+        ],
+    ],
+    'indices' => [
+        [
+            'name'   => 'folders_relpk',
+            'type'   => 'primary',
+            'fields' => ['id'],
+        ],
+        [
+            'name'   => 'idx_poly_module_poly_id',
+            'type'   => 'index',
+            'fields' => ['polymorphic_module', 'polymorphic_id'],
+        ],
+        [
+            'name'   => 'idx_fr_id_deleted_poly',
+            'type'   => 'index',
+            'fields' => ['folder_id', 'deleted', 'polymorphic_id'],
+        ],
+    ],
+];
