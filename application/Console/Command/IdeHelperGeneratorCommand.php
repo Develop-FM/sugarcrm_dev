f0867ccfb1de22742d792de8a12adeed7e115d64
Butschster <butschster@gmail.com>
2016-01-18 12:31:58 +0300

    Update ideHelper

--- application/Console/Command/IdeHelperGeneratorCommand.php
+++ application/Console/Command/IdeHelperGeneratorCommand.php
@@ -7,11 +7,6 @@ use DirectoryIterator;
 use SugarRelationshipFactory;
 use Illuminate\Console\Command;
 
-require_once DOCROOT.'include/utils.php';
-require_once DOCROOT.'include/utils/sugar_file_utils.php';
-require_once DOCROOT.'data/Relationships/RelationshipFactory.php';
-require_once DOCROOT.'include/SugarObjects/VardefManager.php';
-
 class IdeHelperGeneratorCommand extends Command
 {
     /**
@@ -28,142 +23,9 @@ class IdeHelperGeneratorCommand extends Command
      */
     protected $description = 'Генерация вспомогательного файла для IDE';
 
-    /**
-     * @param array $field
-     *
-     * @return string|void
-     */
-    protected function addRealationField($field)
-    {
-        if (($type = Arr::get($field, 'type', 'string')) != 'relate') {
-            return;
-        }
-
-        $idName = Arr::get($field, 'id_name');
-        $link   = Arr::get($field, 'link');
-
-        if (empty($link) or empty($idName)) {
-            return;
-        }
-
-        return "\t\t'{$idName}' => '{$link}',".PHP_EOL;
-    }
-
-    /**
-     * @param array $field
-     *
-     * @return string
-     */
-    protected function addField($field)
-    {
-        $name        = Arr::get($field, 'name');
-        $type        = $this->getType($field);
-        $description = Arr::get($field, 'comment', '');
-
-        if (Arr::get($field, 'type') == 'upload') {
-            $description .= ' ['.Arr::get($field, 'valid_types').']';
-        }
-
-        return "\t/** \n\t * @var {$type} {$description}\n\t */\n\t public \${$name};".PHP_EOL.PHP_EOL;
-    }
-
-    /**
-     * @param array $field
-     *
-     * @return string
-     */
-    protected function addProperty($field)
-    {
-        $type = $this->getType($field);
-        $name = Arr::get($field, 'name');
-
-        $description = Arr::get($field, 'comment', '');
-
-        if (Arr::get($field, 'type') == 'upload') {
-            $description .= ' ['.Arr::get($field, 'valid_types').']';
-        }
-
-        return " * @property {$type} {$name} {$description}".PHP_EOL;
-    }
-
-    /**
-     * @param array $field
-     *
-     * @return string
-     */
-    protected function addMethod($field)
-    {
-        $_type = Arr::get($field, 'type');
-        if (! in_array($_type, ['relate', 'link'])) {
-            return;
-        }
-
-        $type = $this->getType($field);
-        $name = ucfirst(Arr::get($field, 'name'));
-
-        if ($_type == 'link') {
-            $class = $this->getBeanByModule(Arr::get($field, 'module', 'SugarBean'));
-            if (empty($class)) {
-                if (! empty($field['side']) and ! empty($field['relationship'])) {
-                    $relationship = SugarRelationshipFactory::getInstance()->getRelationshipDef($field['relationship']);
-
-                    if ($field['side'] == 'left') {
-                        $module = Arr::get($relationship, "relationships.{$field['relationship']}.lhs_module");
-                    } else {
-                        $module = Arr::get($relationship, "relationships.{$field['relationship']}.rhs_module");
-                    }
-                } else {
-                    return;
-                }
-            } else {
-                $module = Arr::get($field, 'module', 'SugarBean');
-            }
-
-            $type = $this->getBeanByModule($module).'[]';
-        }
-
-        return " * @method {$type} getRelated{$name}".PHP_EOL;
-    }
-
-    /**
-     * @param array $field
-     *
-     * @return string
-     */
-    protected function getType($field)
-    {
-        $type = Arr::get($field, 'type', 'string');
-
-        switch ($type) {
-            case 'bool':
-                return 'bool';
-            case 'link':
-                return load_link_class($field);
-            case 'relate':
-                return $this->getBeanByModule(Arr::get($field, 'module', 'SugarBean'));
-        }
-
-        return 'string';
-    }
-
-    /**
-     * @param string $module
-     *
-     * @return string
-     */
-    protected function getBeanByModule($module)
-    {
-        global $beanList;
-
-        return Arr::get($beanList, $module);
-    }
-
-    /**
-     * @return null
-     */
     public function handle()
     {
-        $path = DOCROOT.'cache' . DIRECTORY_SEPARATOR . 'modules';
+        $path = DOCROOT.'cache'.DIRECTORY_SEPARATOR.'modules';
 
         $helperFile = DOCROOT.'_ide_helper.php';
 
@@ -245,5 +107,135 @@ class IdeHelperGeneratorCommand extends Command
         }
 
         file_put_contents($helperFile, $phpDoc);
+    }
+
+    /**
+     * @param array $field
+     *
+     * @return string|void
+     */
+    protected function addRealationField($field)
+    {
+        if (($type = array_get($field, 'type', 'string')) != 'relate') {
+            return;
+        }
+
+        $idName = array_get($field, 'id_name');
+        $link   = array_get($field, 'link');
+
+        if (empty($link) or empty($idName)) {
+            return;
+        }
+
+        return "\t\t'{$idName}' => '{$link}',".PHP_EOL;
+    }
+
+    /**
+     * @param array $field
+     *
+     * @return string
+     */
+    protected function addField($field)
+    {
+        $name        = array_get($field, 'name');
+        $type        = $this->getType($field);
+        $description = array_get($field, 'comment', '');
+
+        if (array_get($field, 'type') == 'upload') {
+            $description .= ' ['.array_get($field, 'valid_types').']';
+        }
+
+        return "\t/** \n\t * @var {$type} {$description}\n\t */\n\t public \${$name};".PHP_EOL.PHP_EOL;
+    }
+
+    /**
+     * @param array $field
+     *
+     * @return string
+     */
+    protected function addProperty($field)
+    {
+        $type = $this->getType($field);
+        $name = array_get($field, 'name');
+
+        $description = array_get($field, 'comment', '');
+
+        if (array_get($field, 'type') == 'upload') {
+            $description .= ' ['.array_get($field, 'valid_types').']';
+        }
+
+        return " * @property {$type} {$name} {$description}".PHP_EOL;
+    }
+
+    /**
+     * @param array $field
+     *
+     * @return string
+     */
+    protected function addMethod($field)
+    {
+        $_type = array_get($field, 'type');
+        if (! in_array($_type, ['relate', 'link'])) {
+            return;
+        }
+
+        $type = $this->getType($field);
+        $name = ucfirst(array_get($field, 'name'));
+
+        if ($_type == 'link') {
+            $class = $this->getBeanByModule(array_get($field, 'module', 'SugarBean'));
+            if (empty($class)) {
+                if (! empty($field['side']) and ! empty($field['relationship'])) {
+                    $relationship = SugarRelationshipFactory::getInstance()->getRelationshipDef($field['relationship']);
+
+                    if ($field['side'] == 'left') {
+                        $module = array_get($relationship, "relationships.{$field['relationship']}.lhs_module");
+                    } else {
+                        $module = array_get($relationship, "relationships.{$field['relationship']}.rhs_module");
+                    }
+                } else {
+                    return;
+                }
+            } else {
+                $module = array_get($field, 'module', 'SugarBean');
+            }
+
+            $type = $this->getBeanByModule($module).'[]';
+        }
+
+        return " * @method {$type} getRelated{$name}".PHP_EOL;
+    }
+
+    /**
+     * @param array $field
+     *
+     * @return string
+     */
+    protected function getType($field)
+    {
+        $type = array_get($field, 'type', 'string');
+
+        switch ($type) {
+            case 'bool':
+                return 'bool';
+            case 'link':
+                return load_link_class($field);
+            case 'relate':
+                return $this->getBeanByModule(array_get($field, 'module', 'SugarBean'));
+        }
+
+        return 'string';
+    }
+
+    /**
+     * @param string $module
+     *
+     * @return string
+     */
+    protected function getBeanByModule($module)
+    {
+        global $beanList;
+
+        return array_get($beanList, $module);
     }
 }
